b803c051c2bb0b66c90efe682fa957aefce87616
Микола Мантицький <slwpke447@gmail.com>
2019-04-16 11:57:31 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.CallbackNS;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ClientCallback cc = new ClientCallback())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    cc.CallService(i);
+                }
+                Console.Read();
+
+            }
+        }
+
+    }
+    class ClientCallback : IService1Callback, IDisposable
+    {
+        Service1Client client;
+        public void CallbackFunction(string message)
+        {
+            Console.WriteLine($" Msg from Server {message}");
+        }
+
+        public void CallService(int y)
+        {
+            InstanceContext ic = new InstanceContext(this);
+            client = new Service1Client(ic);
+            client.UselessFunction(y);
+        }
+        public void Dispose()
+        {
+            client.Close();
+        }
+    }
+}
---
+++ WcfService2/IService1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(CallbackContract =typeof(IServiceCallback))]
+    public interface IService1
+    {
+        [OperationContract(IsOneWay = true)]
+        void UselessFunction(int x);
+    }
+    public interface IServiceCallback
+    {
+        [OperationContract(IsOneWay =true)]
+        void CallbackFunction(string message);
+    }
+  
+    
+}
